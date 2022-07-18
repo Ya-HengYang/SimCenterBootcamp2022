f32af87569b0c7ef1c1b44b5377f3abb38483a04
Ya-Heng Yang <yaheng@login4.frontera.tacc.utexas.edu>
2022-07-18 13:20:42 -0500

    my practice

--- code/c/ExerciseDay1/solveQuadratic.c
+++ code/c/ExerciseDay1/solveQuadratic.c
@@ -20,6 +20,21 @@ int main(int argc, char **argv) {
   float a = atof(argv[1]);
   float b = atof(argv[2]);
   float c = atof(argv[3]);
+  
+  double coef1 = b*b - 4*a*c;
+  double coef2 = -b/(2*a);
+
+  if (coef1 >= 0.0) {
+    
+    float coef3 = sqrt(coef1)/(2*a);
+    float x1 = coef2 - coef3;
+    float x2 = coef2 + coef3;
+    printf("We have real solution x1: %f  x3   %f\n",x1,x2);
+  } else {
+    float coef3 = sqrt(-coef1)/(2*a);
+    printf("We have imaginary solution x1 %f + %1fi, x2 %f + %fi\n",coef2,-coef3,coef2,coef3);
+  }
+
 
   printf("Have a Nice Day!\n");
   return 0;
